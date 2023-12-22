d3a5e2741aff9cbb5bfc18fa5282af038f567f7e
IT21221064 <99318000+IT21221064@users.noreply.github.com>
2023-12-22 21:55:52 +0530

    Adding Connection

--- BulkyWeb/Data/ApplicationDbContext.cs
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -4,7 +4,7 @@ namespace BulkyWeb.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext()
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options) : base(options)
         {
             
         }
