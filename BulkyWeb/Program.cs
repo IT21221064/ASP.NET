5e401f0c1fe7f38e32b0334351f168e8c6932007
IT21221064 <99318000+IT21221064@users.noreply.github.com>
2023-12-22 22:03:19 +0530

    adding config

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -5,7 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer());
+builder.Services.AddDbContext<ApplicationDbContext>(options => 
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 
 var app = builder.Build();
