614333d694075cdd6881b64cba5f6042bea898fe
IT21221064 <99318000+IT21221064@users.noreply.github.com>
2023-12-27 21:54:44 +0530

    Display categories

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -13,8 +13,8 @@ namespace BulkyWeb.Controllers
         }
         public IActionResult Index()
         {
-            List <Category> objCategoryList = _db.Categories.ToList();
-            return View();
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
         }
     }
 }
