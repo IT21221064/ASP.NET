b09290be2739fb684f0cff451c6da14d58e802bd
IT21221064 <99318000+IT21221064@users.noreply.github.com>
2024-01-06 20:50:24 +0530

    Notifications added

--- BulkyWebRazor_Temp/Pages/Categories/Delete.cshtml.cs
+++ BulkyWebRazor_Temp/Pages/Categories/Delete.cshtml.cs
@@ -34,7 +34,7 @@ namespace BulkyWebRazor_Temp.Pages.Categories
             }
             _db.Categories.Remove(obj);
             _db.SaveChanges();
-         //   TempData["success"] = "Category deleted successfully";
+            TempData["success"] = "Category deleted successfully";
             return RedirectToPage("Index");
         }
     }
