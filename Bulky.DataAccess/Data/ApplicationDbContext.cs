1414f339fd923e67286fa4e14e8aa75acead0f31
IT21221064 <99318000+IT21221064@users.noreply.github.com>
2024-01-15 21:40:41 +0530

    add identityDbContext toDbContext

--- Bulky.DataAccess/Data/ApplicationDbContext.cs
+++ Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
 ﻿using Bulky.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Bulky.DataAccess.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options) : base(options)
         {
@@ -14,6 +15,7 @@ namespace Bulky.DataAccess.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);     //to use IdentityDbContext
             modelBuilder.Entity<Category>().HasData(
                 new Category { Id=1,Name="Action",DisplayOrder=1},
                 new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
