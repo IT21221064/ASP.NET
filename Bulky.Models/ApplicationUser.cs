f3712302aa66d858d4322ab69561afefa1048f5d
IT21221064 <99318000+IT21221064@users.noreply.github.com>
2024-01-16 12:25:52 +0530

    Application Users Added

---
+++ Bulky.Models/ApplicationUser.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        public int Name {  get; set; }
+
+        public string? StreetAddress {  get; set; }
+        public String? City { get; set; }
+        public String? State { get; set; }
+        public String? PostalCode { get; set;}
+    }
+}
